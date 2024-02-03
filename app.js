b78f0c12fb8b2c277106f366b43e0ddf36c22ed0
jaoxmb <joaovictorcgdossantos@gmail.com>
2024-02-02 23:48:40 -0300

    Feat: added ResponsiveVoice.js and code refactoring

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const attemptsBox = document.querySelector('.attempts');
 const attempts = document.querySelector('#attempts-container');
 const title = document.querySelector('#title');
 const statusBar = document.querySelector('#status');
+const speakBtn = document.querySelector('#speak');
 
 const game = {
   max: 100,
   secret: null,
-  attempts: [],
+  attempts: 0,
   status: '',
+  speak: false,
 }
 
 // Game Form
@@ -22,17 +24,15 @@ gameForm.addEventListener('submit', e => {
   const input = form.querySelector('input');
   const value = parseInt(input.value);
   
-  if (game.status === '' || game.status === 'completed') {
+  if (game.status != 'start') {
     start();
     return;
   }
-  
-  if (game.status === 'start') {
-    pushAttempts(value);
-    checkNumber(value);
-    form.reset();
-    input.focus();
-  }
+
+  pushAttempts(value);
+  checkNumber(value);
+  form.reset();
+  input.focus();
 });
 
 // Validity Input Value
@@ -40,11 +40,20 @@ input.addEventListener('input', (e) => {
   const value = parseInt(e.currentTarget.value);
 
   if (!(value >= 0 && value <= game.max)) {
-    input.setCustomValidity(`O valor digitado está fora do intervalo de 0 a ${game.max}.`);
+    const message = `O valor digitado está fora do intervalo de 0 a ${game.max}.`;
+
+    input.setCustomValidity(message);
+    speak(message);
+
     return;
   } 
 
   input.setCustomValidity('');
+});
+
+speakBtn.addEventListener('click', () => {
+  speakBtn.classList.toggle('disabled');
+  game.speak ? game.speak = false : game.speak = true;
 });
 
 function checkNumber(number) {
@@ -52,43 +61,43 @@ function checkNumber(number) {
     bodyFeedback(false);
 
     if (compare > parseInt(game.max / 5)) {
-      title.textContent = `Passou longe! Mas não desista`;
+      write(title, `Passou longe! Mas não desista`);
       return;
     }
 
     if (compare > parseInt(game.max / 10)) {
-      title.textContent = 'As coisas estão começando a esquentar!';
+      write(title, 'As coisas estão começando a esquentar!');
       return;
     }
     
     if (compare < 4) {
-      title.textContent = 'Passou raspando feito um tiro!';
+      write(title, 'Passou raspando feito um tiro!');
     }
   }
   const victoryMessage = (attemptsQuanty) => {
     bodyFeedback(true);
 
     if (attemptsQuanty === 1) {
-      statusBar.textContent = `Deus! Me conte, como você fez isso? Você acertou com ${attemptsQuanty} tentativa.`;
+      write(statusBar, `Deus! Me conte, como você fez isso? Você acertou com ${attemptsQuanty} tentativa.`);
       return;
     }
     if (attemptsQuanty > parseInt(game.max / 8)) {
-      statusBar.textContent = `Foi difícil mas conseguiu, você acertou com ${attemptsQuanty} tentativas.`;
+      write(statusBar, `Foi difícil mas conseguiu, você acertou com ${attemptsQuanty} tentativas.`);
       return;
     }
     if (attemptsQuanty > parseInt(game.max / 12)) {
-      statusBar.textContent = `Sortudo! Sua estratégia é boa, você acertou com ${attemptsQuanty} tentativas.`;
+      write(statusBar, `Sua estratégia é boa, você acertou com ${attemptsQuanty} tentativas.`);
       return;
     }
     if (attemptsQuanty) {
-      statusBar.textContent = `Acima da média! Você acertou com ${attemptsQuanty} tentativas.`;
+      write(statusBar, `Acima da média! Você acertou com ${attemptsQuanty} tentativas.`);
     }
   }
 
   if (number === game.secret) {
-    const attemptsQuanty = game.attempts.length;
+    const attemptsQuanty = game.attempts;
 
-    title.textContent = `Você acertou, parabéns!`;
+    write(title, `Você acertou, parabéns!`);
     victoryMessage(attemptsQuanty);
     completed();
 
@@ -96,34 +105,62 @@ function checkNumber(number) {
   }
   if (number > game.secret) {
     const compare = number - game.secret;
-    statusBar.innerHTML = `O número secreto é <span>menor</span> do que ${number}.`;
+    write(statusBar, `O número secreto é <span>menor</span> do que ${number}.`);
     errorMessage(compare);
 
     return;
   }
   if (number < game.secret) {
     const compare = game.secret - number;
-    statusBar.innerHTML = `O número secreto é <span>maior</span> do que ${number}.`;
+    write(statusBar, `O número secreto é <span>maior</span> do que ${number}.`);
     errorMessage(compare);
 
     return;
   }
 }
-
 function generateRandomNumber() {
   game.secret = parseInt((Math.random() * game.max));
 }
-
 function pushAttempts(number) {
-  game.attempts.push(number);
+  game.attempts++
 
   attempts.innerHTML += `
     <li>${number}</li>
   `;
 }
+function start() {
+  input.classList.add('active');
+  input.removeAttribute('disabled');
+  attemptsBox.classList.remove('disabled');
+  document.body.classList.remove('correct');
 
+  button.textContent = 'Chutar';
+  write(title, 'Bora para o Jogo!');
+  write(statusBar, `Já está valendo! Digite na caixa de entrada um valor de 0 a ${game.max}, tente a sorte!`);
+  attempts.innerHTML = '';
+  
+  game.attempts = 0;
+  game.status = 'start';
+
+  generateRandomNumber();
+}
+function completed() {
+  game.status = 'completed';
+  button.textContent = 'Jogar novamente';
+
+  setTimeout(() => {
+    reset();
+  }, 1000)
+}
+function reset() {
+  input.classList.remove('active');
+  input.setAttribute('disabled', true);
+}
+function loadDOM() {
+  attemptsBox.classList.add('disabled');
+  write(statusBar, `Tente a sorte tentando adivinhar números aleatórios de 0 a ${game.max}.`);
+}
 function bodyFeedback(isCorrect) {
-
   if (isCorrect) {
     document.body.classList.add('correct');
     return;
@@ -132,43 +169,17 @@ function bodyFeedback(isCorrect) {
   document.body.classList.add('incorrect');
   setTimeout(() => {
     document.body.classList.remove('incorrect');
-  }, 1000)
+  }, 1000);
 }
-
-function start() {
-  input.classList.add('active');
-  input.removeAttribute('disabled');
-  button.textContent = 'Chutar';
-  title.textContent = 'Bora para o Jogo!';
-  statusBar.textContent = `Já está valendo! Digite na caixa de entrada um valor de 0 a ${game.max}, tente a sorte!`;
-  attemptsBox.classList.remove('disabled');
-  attempts.innerHTML = '';
-  document.body.classList.remove('correct');
-  
-  game.attempts = [];
-  game.status = 'start';
-
-  generateRandomNumber();
+function write(element, text) {
+  element.innerHTML = text;
+  speak(text);
 }
-
-function completed() {
-  game.status = 'completed';
-
-  setTimeout(() => {
-    reset();
-  }, 1000)
-}
-
-function reset() {
-  button.textContent = 'Iniciar jogo';
-  input.classList.remove('active');
-  input.setAttribute('disabled', true);
-}
-
-function loadDOM() {
-  attemptsBox.classList.add('disabled');
-  title.textContent = 'Jogo da Adivinhação';
-  statusBar.textContent = `Tente a sorte tentando adivinhar números aleatórios de 0 a ${game.max}.`;
+function speak(text) {
+  if (game.speak) {
+    const formatedText = text.replace(/<[^>]*>/g, '');
+    responsiveVoice.speak(formatedText, "Brazilian Portuguese Female", {rate: 1.3});
+  }
 }
 
 reset();
